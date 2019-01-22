a23f40dae581c6c29a6b232f97e4fdd471a6cede
PeterCassell92 <pc0122@my.bristol.ac.uk>
2019-01-22 19:52:41 +0000

    First Exercise - v. easy

---
+++ ex1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+def yes_or_no(question):
+    reply = str(raw_input(question+' (y/n): ')).lower().strip()
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
+    else:
+        return yes_or_no("Uhhhh... please enter ")
+
+name = raw_input("What is your name?")
+print("What is your age?")
+age = input()
+
+if yes_or_no("Have you had your birthday this year?"):
+	correction = 0
+else:
+	correction = 1
+
+
+
+
+centenary = (2019 -age - correction +100)
+print("You will turn 100 in the year " + str(centenary))
